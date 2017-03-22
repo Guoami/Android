f28ef2dc89ff7ad344064a652e7e099d389df7e7
郭义贤 <guoami@outlook.com>
2017-03-22 19:09:07 +0800

    2017.3.22 OBR

--- UILayoutTest/app/src/main/java/guoami/uilayouttest/MainActivity.java
+++ UILayoutTest/app/src/main/java/guoami/uilayouttest/MainActivity.java
@@ -1,7 +1,12 @@
 package guoami.uilayouttest;
 
+import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.hide();
+        }
     }
 }
+
