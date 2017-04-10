ae6b0db9cdb6f81d5bf7e0d3815aee92e1384c32
郭义贤 <guoami@outlook.com>
2017-04-10 14:13:09 +0800

    2017.4.10 OBR

--- UITask/app/src/main/java/com/guoami/uitask/BottomBehavior.java
+++ UITask/app/src/main/java/com/guoami/uitask/BottomBehavior.java
@@ -3,8 +3,10 @@ package com.guoami.uitask;
 import android.content.Context;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.VelocityTrackerCompat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 
 import static android.R.attr.flipInterval;
@@ -15,6 +17,9 @@ import static android.R.attr.translationY;
  */
 
 public class BottomBehavior extends CoordinatorLayout.Behavior<View> {
+
+    private VelocityTracker mVelocityTracker = null;
+    private float translationY;
 
 
     public BottomBehavior(Context context, AttributeSet attrs) {
@@ -29,15 +34,45 @@ public class BottomBehavior extends CoordinatorLayout.Behavior<View> {
 
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
-        float translationY = Math.abs(dependency.getTop());
-
-        child.setTranslationY(translationY);
+        translationY = Math.abs(dependency.getTop());
+//        child.setTranslationY(translationY);
         return true;
     }
 
     @Override
-    public boolean onTouchEvent(CoordinatorLayout parent, View child, MotionEvent motionEvent) {
-        child.setTranslationY(100);
+    public boolean onTouchEvent(CoordinatorLayout parent, View child, MotionEvent event) {
+        int index = event.getActionIndex();
+        int action = event.getActionMasked();
+        int pointerId = event.getPointerId(index);
+        switch(action) {
+            case MotionEvent.ACTION_DOWN:
+                if(mVelocityTracker == null) {
+                    // Retrieve a new VelocityTracker object to watch the velocity of a motion.
+                    mVelocityTracker = VelocityTracker.obtain();
+                }
+                else {
+                    // Reset the velocity tracker back to its initial state.
+                    mVelocityTracker.clear();
+                }
+                // Add a user's movement to the tracker.
+                mVelocityTracker.addMovement(event);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mVelocityTracker.addMovement(event);
+                // When you want to determine the velocity, call
+                // computeCurrentVelocity(). Then call getXVelocity()
+                // and getYVelocity() to retrieve the velocity for each pointer ID.
+                mVelocityTracker.computeCurrentVelocity(1000);
+                // Log velocity of pixels per second
+                // Best practice to use VelocityTrackerCompat where possible.
+                VelocityTrackerCompat.getYVelocity(mVelocityTracker,pointerId);
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                // Return a VelocityTracker object back to be re-used by others.
+                mVelocityTracker.recycle();
+                break;
+        }
         return true;
     }
 }
